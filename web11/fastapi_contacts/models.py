86797f3760ec095a884075604bf469e967eb2ffe
Анастасия <86166922+BogatNahh@users.noreply.github.com>
2025-04-03 16:54:21 +0300

    Add files via upload

---
+++ web11/fastapi_contacts/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Date
+from fastapi_contacts.database import Base
+from database import Base
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String, unique=True, index=True)
+    birthday = Column(Date)
+    extra_info = Column(String, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
